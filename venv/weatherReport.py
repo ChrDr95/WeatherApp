4985658cecf35549e9e56160032b2e4522a570b0
Christian Diaz <cdr03394@gmail.com>
2018-10-15 13:23:00 +0200

    First(no venv)

--- venv/weatherReport.py
+++
@@ -1,7 +0,0 @@
-class WeatherReport:
-    def _init_(self,temperature,humidity,pressure):
-        self.temperature=temperature
-        self.humidity=humidity
-        self.pressure=pressure
-    def toJSON(self):
-        return {'Temperature: ':self.temperature,'<br /> Humidity: ':self.humidity,'<br /> Pressure: ':self.pressure}
