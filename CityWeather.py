3dadc48dc3d57f93cc64cfba08de19dc73d71e3c
Christian Diaz <cdr03394@gmail.com>
2018-10-24 21:24:43 +0200

    Date: Wednesday Oct 24 21:21
        Making code easier to read
    
        Add 'CityWeather' class

---
+++ CityWeather.py
@@ -0,0 +1,5 @@
+class CityWeather:
+    def __init__(self,temperature,humidity,pressure):
+        self.temperature = temperature
+        self.humidity = humidity
+        self.pressure = pressure
--- app.py
+++ app.py
@@ -1,44 +1,52 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask,jsonify
-from urllib.request import urlopen as uReq
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 import re
+import CityWeather
 app = Flask(__name__)
+WEATHER_URL = 'https://www.timeanddate.com/weather/'
 
 
 @app.route('/')
 def home():
-
     return "Please insert first the name of the Country and then the city <br /> Example:Guatemala/Guatemala"
 
 
 @app.route('/<string:country>/<string:city>')
-def city_name(country,city):
-    try:
-        # Get the weather page to scrap the info (adding the info of the country and city to know the page to access)
-        city_web = 'https://www.timeanddate.com/weather/'+country+"/"+city
-        uClient = uReq(city_web)
-        for_soup = uClient.read()
-        page_soup = soup(for_soup, "html.parser")
-        # Scrap the Temperature
-        temp = page_soup.find_all("div", {"id": "qlook"})[0].find_all('div')
-        # Scrap Humidity and Pressure
-        info_weather = page_soup.find_all("div", {"id": "qfacts"})[0].find_all('p')
-        # turn into normal text so it becomes easier to read
-        temperature = temp[1].text
-        pressure = info_weather[4].text
-        humidity = info_weather[5].text
-        # Change the text into integers
-        tempint=int(re.search(r'\d+',temperature).group())
-        preint=int(re.search(r'\d+',pressure).group())
-        humint=int(re.search(r'\d+',humidity).group())
+def get_weather(country, city):
+    report = get_report(country, city)
+    return jsonify(report)
 
-        report= {'Temperature: ':tempint,'Humidity: ':humint,'Pressure: ':preint}
+
+def to_num(text):
+    return int(re.search(r'\d+', text).group())
+
+
+def scrape_info(for_soup):
+
+    page_soup = soup(for_soup, "html.parser")
+    temperature = page_soup.find_all("div", {"id": "qlook"})[0].find_all('div')
+    humid_press = page_soup.find_all("div", {"id": "qfacts"})[0].find_all('p')
+    humidity = humid_press[4]
+    pressure = humid_press[5]
+    city_weather = CityWeather(temperature, humidity, pressure)
+    return city_weather
+
+
+def get_weather_url(country, city):
+    return WEATHER_URL + country + "/" + city
+
+
+def get_report(country,city):
+    try:
+        weather_url = get_weather_url(country, city)
+        request_website = urlopen(weather_url)
+        report = scrape_info(request_website.read())
     except:
         print("Error: City couldn't be found")
-        report=" Couldn't find the requested city"
-    return jsonify({('Weather '+country+' '+city):report})
+    return report
 
 
 if __name__ == '__main__':
